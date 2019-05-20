18807ecb62d45774a248ffb910e02d2290d501ab
Lauro Franco Neto <laurinho20@gmail.com>
2019-05-20 00:44:18 -0300

    projeto_unity

---
+++ vulforia_project/Assets/scripts/button_mech.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+public class button_mech : MonoBehaviour
+{
+    //possiveis botões
+    public Button cartao;
+    public Button desenho;
+    public Button camiseta;
+    public Button redownload;
+    // file
+    private WWW www;
+    //canvas 
+    public Canvas canvasprimario;
+    public Canvas canvassecundario;
+    public Canvas canvaserror;
+    // Barra de progresso do download
+    public Slider barradedownload;
+    //booleanas
+    public bool loadinON;
+    public bool instalacaoON = true;
+
+
+    void Start()
+    {
+        //controle dos huds
+        barradedownload.maxValue = 100;
+        canvasprimario.enabled = true;
+        canvassecundario.enabled = false;
+        canvaserror.enabled = false;
+        //mecanica dos botões
+        cartao.onClick.AddListener(OnClick);
+        redownload.onClick.AddListener(errordown);
+        camiseta.onClick.AddListener(OtherClick);
+        desenho.onClick.AddListener(OtherClick);
+    }
+
+    void Update()
+    {
+        //controle da barra de progresso
+        if (loadinON)
+            loading();
+        //caso tenha que instalar o arquivo
+        if (instalacaoON)
+        {
+            //se a instalação for concluida
+            if (www.isDone)
+            {
+                instalacao();
+                instalacaoON = false;
+                StartCoroutine(verificacao());
+            }
+        }
+    }
+    //ação do botão principal
+    void OnClick() {
+        //caso o arquivo ja esteja no aparelho
+        if (System.IO.File.Exists(Application.persistentDataPath + "/video.mp4"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else //caso não esteja
+        {
+            www = new WWW("https://whatsnextdigital.com.br/cdn/Shared/videos/whatsnextdigital_video.mp4");
+            canvasprimario.enabled = false;
+            canvassecundario.enabled = true;
+            loadinON = true;
+        }
+        }
+    //ação dos outros botões
+    void OtherClick() {
+        //colocar a ação aqui
+    }
+    //controle da barra de progresso (apenas executa caso seja necessario)
+    void loading() {
+        barradedownload.value = www.progress *100;
+    }
+    //intalação do video
+    void instalacao() {
+        System.IO.File.WriteAllBytes(Application.persistentDataPath + "/video.mp4", www.bytes);
+    }
+    //verificando se baixou o arquivo
+    IEnumerator verificacao()
+    {
+        yield return new WaitForSeconds(4f);
+        //caso o arquivo esteja lá
+        if (System.IO.File.Exists(Application.persistentDataPath + "/video.mp4"))
+        {
+            canvasprimario.enabled = true;
+            canvassecundario.enabled = false;
+            canvaserror.enabled = false;
+        }
+        else {
+            canvasprimario.enabled = false;
+            canvassecundario.enabled = false;
+            canvaserror.enabled = true;
+            //Debug.Log(www.error); descobrir o erro
+        }
+
+    }
+    //ação do botão de redownload (caso necessario)
+    void errordown() {
+        www = new WWW("https://whatsnextdigital.com.br/cdn/Shared/videos/whatsnextdigital_video.mp4");
+        canvasprimario.enabled = false;
+        canvassecundario.enabled = true;
+        canvaserror.enabled = false;
+        loadinON = true;
+        instalacaoON = true;
+    }
+}
---
+++ vulforia_project/Assets/scripts/video_config.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using System.IO;
+
+public class video_config : MonoBehaviour
+{
+  private Renderer rend;
+  private VideoPlayer videos;
+    private bool voltou;
+    public string url;
+
+    private bool teste = true;
+    void Start()
+
+    {
+        //recolhendo componentes
+        videos = GetComponent<VideoPlayer>();
+        rend = GetComponent<Renderer>();
+        //path do video
+        url = Application.persistentDataPath + "/video.mp4";
+        //colocando a path
+        if (File.Exists(url))
+            videos.url = "file://" + url;
+        //else caso não tenha o video (pode voltar para a primeira cena para baixar novamente ou mostrar um aviso de error)
+    }
+
+    void Update()
+    {
+        //caso o obj esteja visivel
+        if (rend.isVisible)
+        {
+            if (voltou)
+            {
+                //tocar automaticamente 
+                 videos.Play();
+                voltou = false;
+            }
+            //caso queira pausar ou recomeçar no touch
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    if (videos.isPlaying)
+                        videos.Pause();
+                    else
+                        videos.Play();
+                }
+            }
+        }
+        else //caso não
+        {
+            voltou = true;
+            videos.Pause();
+        }  
+        }
+}
